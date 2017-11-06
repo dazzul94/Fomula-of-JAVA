3ddaa17e8db0aadf2e7de1ffccd9834a2882659e
dazzul94 <dazzul@naver.com>
2017-11-06 10:45:50 +0900

    .

--- src/main/java/ch06/PrimitiveParamEx.java
+++ src/main/java/ch06/PrimitiveParamEx.java
@@ -3,15 +3,16 @@ class Data { int x;}
 
 class PrimitiveParamEx {
 
+    static void change(int x) {
+        x = 1000;
+        System.out.println("change() : x = " + x);
+    }
+    
     public static void main(String[] args) {
         Data d = new Data();
         d.x = 10;
         System.out.println("main() : x = " + d.x);
-        change(d);
+        change(d.x);
         System.out.println("After change(): x =" + d.x);
-    }
-    static void change(Data d) {
-        d.x = 1000;
-        System.out.println("change() : x = " + d.x);
     }
 }
